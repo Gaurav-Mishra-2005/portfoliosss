85686eec06dc3dce71953c3c6b8f954065b654e1
Gaurav Mishra <gaurav769786@gmail.com>
2025-09-03 21:26:46 +0530

    Updated About, Hero, and project data components

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -5,14 +5,14 @@ export const projectsData: Project[] = [
     title: 'Portfolio Website',
     description: 'A responsive React portfolio with theme switching functionality, optimized for mobile and desktop.',
     technologies: ['React', 'TypeScript', 'Tailwind CSS'],
-    githubLink: 'https://github.com/gauravmishra-here/portfolio',
+    githubLink: 'https://github.com/Gaurav-Mishra-2005/portfoliosss/tree/main',
     demoLink: 'https://gauravmishra-portfolio.vercel.app'
   },
   {
     title: 'ScholarSync',
     description: 'A Scholarship Discovery & Deadline Reminder Platform ',
-    technologies: ['React', 'Tailwind CSS', 'Node.js', 'Express', 'MongoDB', 'Firebase', 'NodeMailer'],
-    githubLink: 'https://github.com/gauravmishra-here/task-manager',
+    technologies: ['React', 'Tailwind CSS, TypeScript'],
+    githubLink: 'https://github.com/Gaurav-Mishra-2005/ScholarSync',
     demoLink: 'https://gauravmishra-task-manager.vercel.app'
   },
   {
